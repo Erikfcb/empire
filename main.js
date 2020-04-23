c515d9f0f3b1c1da4239985e084ebae6f15f154a
Erik Vardanyan <erikfcb@gmail.com>
2020-04-23 18:10:24 +0300

    added app initiallizer

---
+++ main.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "./build")));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("server is running on port " + PORT);
+});
